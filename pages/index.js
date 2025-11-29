ca8ce6ce12baf85aa36ce8a76dfd63ae5ab4f5b7
jkydd13 <Sharkmeatlove88@gmail.com>
2025-11-28 22:21:56 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
-import Header from "../components/Header";
-
 export default function Home() {
   return (
-    <>
-      <Header />
-
-      <main style={{ maxWidth: "700px", margin: "0 auto", padding: "20px" }}>
-        <h1>Welcome to YardHub</h1>
-        <p>Welcome to the neighborhood.</p>
-      </main>
-    </>
+    <main style={{ maxWidth: "700px", margin: "0 auto", padding: "20px" }}>
+      <h1>Welcome to YardHub</h1>
+      <p>Welcome to the neighborhood.</p>
+    </main>
   );
 }
